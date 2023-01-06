28620d511a97bdb323423cca2e6cc3648c4cacb2
Anderson Gomes <andersonags.93@gmail.com>
2023-01-06 10:39:27 -0300

    Exemplo 4.9 - Programa Inverte Centena

---
+++ cap04/nodejs/ex4_9.js
@@ -0,0 +1,17 @@
+// c) Elaborar um programa que leia um número - que deve seruma centena. calcule e exiba a centena invertida. Caso o número não seja uma centena, exiba uma mensagem.
+
+const prompt = require("prompt-sync")()
+const numero = Number(prompt("Número (centena): "))
+
+if (numero < 100 || numero >= 1000) {
+    console.log("Erro... deve ser uma centena!")
+    return
+}
+
+
+const num1 = Math.floor(numero / 100)
+const sobra = numero % 100
+const num2 = Math.floor(sobra / 10)
+const num3 = sobra % 10
+
+console.log(`Invertido: ${num3}${num2}${num1}`)
